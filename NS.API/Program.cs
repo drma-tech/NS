041385a1fb87b4a9d535026cbd54a22d36ff83bb
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-12-01 17:33:39 +0700

    ...

--- NS.API/Program.cs
+++ NS.API/Program.cs
@@ -10,68 +10,68 @@ using NS.API.Core.Auth;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
-var app = new HostBuilder()
+try
+{
+    var app = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults(worker =>
     {
         worker.UseMiddleware<ApiMiddleware>();
     })
     .ConfigureAppConfiguration((hostContext, config) =>
     {
-        try
+        if (hostContext.HostingEnvironment.IsDevelopment())
         {
-            if (hostContext.HostingEnvironment.IsDevelopment())
-            {
-                config.AddJsonFile("local.settings.json");
-                config.AddUserSecrets<Program>();
-            }
-
-            var cfg = new Configurations();
-            config.Build().Bind(cfg);
-            ApiStartup.Configurations = cfg;
-
-            var key = ApiStartup.Configurations.Firebase?.PrivateKey ?? throw new NotificationException("PrivateKey null");
-
-            var firebaseConfig = new FirebaseConfig
-            {
-                project_id = "streaming-discovery-4c483",
-                private_key_id = ApiStartup.Configurations.Firebase?.PrivateKeyId ?? throw new NotificationException("PrivateKeyId null"),
-                private_key = Regex.Unescape(key),
-                client_email = ApiStartup.Configurations.Firebase?.ClientEmail ?? throw new NotificationException("ClientEmail null"),
-                client_id = ApiStartup.Configurations.Firebase?.ClientId ?? throw new NotificationException("ClientId null"),
-                client_x509_cert_url = ApiStartup.Configurations.Firebase?.CertUrl ?? throw new NotificationException("Firebase null")
-            };
-
-            var firebaseJson = JsonSerializer.Serialize(firebaseConfig);
-
-            if (FirebaseApp.DefaultInstance == null)
-            {
-                FirebaseApp.Create(new AppOptions
-                {
-                    Credential = GoogleCredential.FromJson(firebaseJson)
-                });
-            }
+            config.AddJsonFile("local.settings.json");
+            config.AddUserSecrets<Program>();
         }
-        catch (Exception ex)
-        {
-            var tempClient = new CosmosClient(ApiStartup.Configurations.CosmosDB?.ConnectionString, new CosmosClientOptions()
-            {
-                SerializerOptions = new CosmosSerializationOptions
-                {
-                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
-                }
-            });
-            var tempRepo = new CosmosLogRepository(tempClient);
-            var provider = new CosmosLoggerProvider(tempRepo);
-            var loggerFactory = LoggerFactory.Create(builder => builder.AddProvider(provider));
-            var logger = loggerFactory.CreateLogger("ConfigureAppConfiguration");
 
-            logger.LogError(ex, "ConfigureAppConfiguration");
+        var cfg = new Configurations();
+        config.Build().Bind(cfg);
+        ApiStartup.Configurations = cfg;
+
+        var key = ApiStartup.Configurations.Firebase?.PrivateKey ?? throw new NotificationException("PrivateKey null");
+
+        var firebaseConfig = new FirebaseConfig
+        {
+            project_id = "streaming-discovery-4c483",
+            private_key_id = ApiStartup.Configurations.Firebase?.PrivateKeyId ?? throw new NotificationException("PrivateKeyId null"),
+            private_key = Regex.Unescape(key),
+            client_email = ApiStartup.Configurations.Firebase?.ClientEmail ?? throw new NotificationException("ClientEmail null"),
+            client_id = ApiStartup.Configurations.Firebase?.ClientId ?? throw new NotificationException("ClientId null"),
+            client_x509_cert_url = ApiStartup.Configurations.Firebase?.CertUrl ?? throw new NotificationException("Firebase null")
+        };
+
+        var firebaseJson = JsonSerializer.Serialize(firebaseConfig);
+
+        if (FirebaseApp.DefaultInstance == null)
+        {
+            FirebaseApp.Create(new AppOptions
+            {
+                Credential = GoogleCredential.FromJson(firebaseJson)
+            });
         }
     })
     .ConfigureServices(ConfigureServices)
     .Build();
 
-await app.RunAsync();
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    var tempClient = new CosmosClient(ApiStartup.Configurations.CosmosDB?.ConnectionString, new CosmosClientOptions()
+    {
+        SerializerOptions = new CosmosSerializationOptions
+        {
+            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+        }
+    });
+    var tempRepo = new CosmosLogRepository(tempClient);
+    var provider = new CosmosLoggerProvider(tempRepo);
+    var loggerFactory = LoggerFactory.Create(builder => builder.AddProvider(provider));
+    var logger = loggerFactory.CreateLogger("ConfigureAppConfiguration");
+
+    logger.LogError(ex, "ConfigureAppConfiguration");
+}
 
 static void ConfigureServices(IServiceCollection services)
 {
