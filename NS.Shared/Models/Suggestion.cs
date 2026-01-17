6173cc4113510cdef3be9aed92bb01745a7510cb
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-01-18 00:17:46 +0700

    suggestion with photos

--- NS.Shared/Models/Suggestion.cs
+++ NS.Shared/Models/Suggestion.cs
@@ -15,5 +15,6 @@
         public string? Code { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
+        public string? PhotoId { get; set; }
     }
 }
