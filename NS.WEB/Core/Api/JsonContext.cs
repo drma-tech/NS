d5f281d123bc102f21de7aff37225ca223d97d09
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-03-06 13:22:53 -0300

    code sync

---
+++ NS.WEB/Core/Api/JsonContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NS.WEB.Core.Api
+{
+    [JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
+    [JsonSerializable(typeof(Platform?))]
+    [JsonSerializable(typeof(AppLanguage?))]
+    [JsonSerializable(typeof(bool?))]
+    [JsonSerializable(typeof(string))]
+    internal partial class JavascriptContext : JsonSerializerContext
+    {
+    }
+}
