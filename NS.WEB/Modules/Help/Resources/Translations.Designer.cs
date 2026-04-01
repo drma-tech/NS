bc314c5580e9f2f20eca7f1bac4b15947ccea5ad
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-04-01 15:09:50 -0300

    support routes review

--- NS.WEB/Modules/Help/Resources/Translations.Designer.cs
+++ NS.WEB/Modules/Help/Resources/Translations.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NS.WEB.Modules.Support.Resources {
+namespace NS.WEB.Modules.Help.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace NS.WEB.Modules.Support.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NS.WEB.Modules.Support.Resources.Translations", typeof(Translations).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NS.WEB.Modules.Help.Resources.Translations", typeof(Translations).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
