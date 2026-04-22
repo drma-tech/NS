90735833b946b3a3707cae54cb56c478e57835f7
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-04-22 11:40:31 -0300

    new translations: italian and french

--- NS.Shared/Resources/Enum/AccountCycle.Designer.cs
+++ NS.Shared/Resources/Enum/AccountCycle.Designer.cs
@@ -19,7 +19,7 @@ namespace NS.Shared.Resources.Enum {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class AccountCycle {
@@ -75,6 +75,24 @@ namespace NS.Shared.Resources.Enum {
         public static string Monthly {
             get {
                 return ResourceManager.GetString("Monthly", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Week.
+        /// </summary>
+        public static string Week {
+            get {
+                return ResourceManager.GetString("Week", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Weekly.
+        /// </summary>
+        public static string Weekly {
+            get {
+                return ResourceManager.GetString("Weekly", resourceCulture);
             }
         }
         
