d7a580eae656c78679d2f7d7dc0284b05c82a4bc
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-05-22 18:36:21 -0300

    suggestion improvement / accomodations list

---
+++ NS.WEB/Modules/Suggestions/Core/SuggestionItem.cs
@@ -0,0 +1,11 @@
+﻿namespace NS.WEB.Modules.Suggestions.Core
+{
+    public class SuggestionItem
+    {
+        public string? Name { get; set; }
+        public string? IconPath { get; set; }
+        public string? IconPath2 { get; set; }
+        public string? Logo { get; set; }
+        public string? Link { get; set; }
+    }
+}
