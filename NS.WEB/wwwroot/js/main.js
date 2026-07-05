506255ac9f8a39634ee4f980a85e364c51380cc7
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-07-05 12:19:10 -0300

    js refactored

--- NS.WEB/wwwroot/js/main.js
+++ NS.WEB/wwwroot/js/main.js
@@ -1,12 +1,9 @@
-﻿const ua = navigator.userAgent;
+﻿"use strict";
 
-window.browser = window?.bowser?.getParser
-    ? window.bowser.getParser(ua)
-    : null;
-
+const ua = navigator.userAgent;
+window.browser = window.bowser?.getParser ? window.bowser.getParser(ua) : null;
 const botUAs = ["google", "baidu", "bingbot", "duckduckbot", "teoma", "slurp", "yandex", "toutiao", "bytespider", "applebot", "crawler"];
-
-export const isBot = botUAs.some(bot => ua.toLowerCase().includes(bot)) || navigator.webdriver;
+const isBot = botUAs.some(bot => ua.toLowerCase().includes(bot)) || navigator.webdriver;
 
 function testBrowserVersion(rules, ignore = false, fallback = false) {
     if (ignore) return false;
@@ -20,21 +17,29 @@ function testBrowserVersion(rules, ignore = false, fallback = false) {
     }
 }
 
-//browser versions not compatible with SIMD
-export const hideBlazorIndex = testBrowserVersion(
-    {
-        chrome: "<91", //may 21
-        edge: "<91", //may 21
-        firefox: "<89", //may 21
-        safari: "<16.4", //mar 23
-        opera: "<77", //jun 21
-    },
-    /Mediapartners-Google/i.test(ua),
-    false // uncertain environment → allow
-);
+const wasmSupported = typeof WebAssembly === "object";
+const isLocalhost = window.location.hostname === "localhost";
+const isPrerendering = window.location.hostname === "127.0.0.1"
+const isDev = location.hostname.includes("develop");
+const isWebview = /webtonative/i.test(ua);
+const isPrintScreen = location.href.includes("printscreen");
+
+function supportsWasmSimd() {
+    if (typeof WebAssembly === "undefined") return false;
+    const bytes = new Uint8Array([
+        0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00,
+        0x01, 0x05, 0x01, 0x60, 0x00, 0x01, 0x7b,
+        0x03, 0x02, 0x01, 0x00,
+        0x0a, 0x0a, 0x01, 0x08, 0x00, 0x41, 0x00, 0xfd, 0x0f, 0xfd, 0x62, 0x0b
+    ]);
+    return WebAssembly.validate(bytes);
+}
+
+//The browser does not support WASM or SIMD.
+const blazorSupported = wasmSupported && supportsWasmSimd();
 
 //probably a bot, so doesnt support sw
-export const disableServiceWorker = testBrowserVersion(
+const disableServiceWorker = testBrowserVersion(
     {
         chrome: "<134", //special case (usually bots)
         edge: "<91", //may 21
@@ -42,25 +47,34 @@ export const disableServiceWorker = testBrowserVersion(
         safari: "<16.4", //mar 23
         opera: "<77", //jun 21
     },
-    false,
+    isWebview,
     true // uncertain environment → disable
 );
 
-export const isLocalhost = location.host.includes("localhost");
-export const isDev = location.hostname.includes("develop");
-export const isWebview = /webtonative/i.test(ua);
-export const isPrintScreen = location.href.includes("printscreen");
-
-export const servicesConfig = {
+const servicesConfig = {
     AnalyticsCode: "G-G8CBVXZDD8",
     ClarityKey: "veo2fcozds",
     UserBackToken: "A-A2J4M5NKCbDp1QyQe7ogemmmq",
     SentryDsn: "https://7ef8d637b418314a675a37d9cc70522c@o4510938040041472.ingest.us.sentry.io/4510943123668992",
 };
 
-export const supabaseConfig = {
+const supabaseConfig = {
     projectUrl: "https://svvydpwqfuoxovuabgap.supabase.co",
     supabaseKey: "sb_publishable_Pp1UvXpyiHBMeaY9wwywaQ_11-ou6_W",
 };
 
-export const baseApiUrl = isLocalhost ? "http://localhost:7262" : "";
+const baseApiUrl = isLocalhost ? "http://localhost:7262" : "";
+
+window.appConfig = {
+    isBot,
+    blazorSupported,
+    disableServiceWorker,
+    isLocalhost,
+    isPrerendering,
+    isDev,
+    isWebview,
+    isPrintScreen,
+    servicesConfig,
+    supabaseConfig,
+    baseApiUrl
+};
