348ac23d67f5bf2367aef49dfe508ac80c6e612f
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-07-08 10:36:56 -0300

    code sync

--- NS.WEB/Core/Helper/PopupHelper.cs
+++ NS.WEB/Core/Helper/PopupHelper.cs
@@ -15,19 +15,19 @@ public static class PopupHelper
     {
         var parameters = new DialogParameters<AccountPopup> { };
 
-        await service.ShowAsync<AccountPopup>(Modules.Auth.Resources.Translations.MyAccount, parameters, Options(MaxWidth.Small));
+        await service.ShowAsync<AccountPopup>(Translations.Module.Auth.MyAccount, parameters, Options(MaxWidth.Small));
     }
 
     public static async Task SettingsPopup(this IDialogService service)
     {
-        await service.ShowAsync<SettingsPopup>(GlobalTranslations.Settings, Options(MaxWidth.Small));
+        await service.ShowAsync<SettingsPopup>(Translations.Module.Help.Settings, Options(MaxWidth.Small));
     }
 
     public static async Task SubscriptionPopup(this IDialogService service)
     {
         var parameters = new DialogParameters<SubscriptionPopup> { };
 
-        await service.ShowAsync<SubscriptionPopup>(Modules.Subscription.Resources.Translations.MySubscription, parameters, Options(MaxWidth.Medium));
+        await service.ShowAsync<SubscriptionPopup>(Translations.Module.Subscription.MySubscription, parameters, Options(MaxWidth.Medium));
     }
 
     public static async Task OnboardingPopup(this IDialogService service, string culture)
@@ -37,12 +37,12 @@ public static class PopupHelper
             { x => x.Culture, culture },
         };
 
-        await service.ShowAsync<Onboarding>(string.Format(GlobalTranslations.WelcomeTo, AppInfo.Title), parameters, Options(MaxWidth.Medium));
+        await service.ShowAsync<Onboarding>(string.Format(Translations.Module.Help.WelcomeTo, AppInfo.Title), parameters, Options(MaxWidth.Medium));
     }
 
     public static async Task AskReviewPopup(this IDialogService service)
     {
-        await service.ShowAsync<AskReview>(string.Format(GlobalTranslations.WriteReviewTitle, AppInfo.Title), Options(MaxWidth.Small, false, false));
+        await service.ShowAsync<AskReview>(string.Format(Translations.Module.Help.WriteReviewTitle, AppInfo.Title), Options(MaxWidth.Small, false, false));
     }
 
     public static DialogOptions Options(MaxWidth width, bool allowClose = true, bool showHeader = true)
--- NS.WEB/Global.cs
+++ NS.WEB/Global.cs
@@ -3,8 +3,8 @@ global using NS.Shared.Core.Helper;
 global using NS.Shared.Core.Models;
 global using NS.Shared.Enums;
 global using NS.Shared.Models;
+
 global using NS.WEB.Core;
 global using NS.WEB.Core.Api;
 global using NS.WEB.Core.Helper;
 global using NS.WEB.Shared.Core;
-global using NS.WEB.Resources;
